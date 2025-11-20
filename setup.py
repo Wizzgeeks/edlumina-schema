d62014781bf2792ef50df84c8fcf91e9a0c86b8e
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-11-20 12:09:05 +0530

    required field changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="edlumina-schema",
-    version="0.0.5",
+    version="0.0.6",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
