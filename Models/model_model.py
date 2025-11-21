eaadd47d41d5d0c4cd798c8e563cf7fe5d13f71b
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-11-21 12:27:40 +0530

    models and prompt table created

---
+++ Models/model_model.py
@@ -0,0 +1,41 @@
+from mongoengine import Document,StringField,ValidationError,BooleanField
+
+class Model(Document):
+    name = StringField(required=True,unique=True)
+    api_key = StringField(required=True)
+    type = StringField(required=True)
+    provider = StringField(choices=['gemini','openai','claude'],required=True)
+    is_active = BooleanField(default=False)
+
+    
+    def clean(self):
+        if not self.name.strip():
+            raise ValidationError("Model name cannot be empty")
+        if not self.api_key.strip():
+            raise ValidationError("Api key cannot be empty")
+        if not self.type.strip():
+            raise ValidationError("model type cannot be empty")
+
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "name":self.name,
+            "type":self.type,
+            "provider":self.provider,
+            "is_active":self.is_active if self.is_active else False
+        }
+    def to_admin(self):
+        return{
+            "id": str(self.id),
+            "name":self.name,
+            "type":self.type,
+            "provider":self.provider,
+            "api_key":self.api_key,
+            "is_active":self.is_active if self.is_active else False
+        }
+    
+        
+        
+    def update(self, **kwargs):
+        self.clean()
+        return super().update(**kwargs)
---
+++ Models/prompt.py
@@ -0,0 +1,33 @@
+from mongoengine import Document,StringField,BooleanField,EnumField,ReferenceField,ListField,DictField,EmbeddedDocument,DateTimeField,IntField,EmbeddedDocumentField
+from datetime import datetime,timezone
+
+
+class Prompt(Document):
+    name = StringField(required=True)
+    prompt=StringField()
+    json_mode=BooleanField(default=False)
+    json_schema=StringField()
+    types=StringField(choices=['mcq','match','fillups','content','expand','update','trueorfalse'],required=True)
+    default=BooleanField(default=False)
+    created_by=StringField()
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now(timezone.utc)
+        return super(Prompt, self).save(*args, **kwargs)
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "prompt": self.prompt if self.prompt else "",
+            "json_schema": self.json_schema if self.json_schema else "",
+            "created_by":self.created_by if self.created_by else "",
+            "json_mode": self.json_mode,
+            "created_at": self.created_at if self.created_at else None,
+            "updated_at": self.updated_at if self.updated_at else None
+        }
+
+
+
+
