ce483bd78d689f1d89738b02669a812502133705
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-11-25 14:18:59 +0530

    new model for batches and changes in the user model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="edlumina-schema",
-    version="0.1.9",
+    version="0.2.0",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
