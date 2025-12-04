1a4abc3f6ef5556fed61b238bbdbd5dff922a3ba
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-12-04 16:53:02 +0530

    new models created for question bank folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="edlumina-schema",
-    version="0.4.9",
+    version="0.5.0",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
