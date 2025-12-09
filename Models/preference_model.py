4f88997bd4278ba8068a4d8aab1f1eb58888ff7f
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2025-12-09 12:52:32 +0530

    new model for prefernce question

---
+++ Models/preference_model.py
@@ -0,0 +1,24 @@
+from mongoengine import Document, DateTimeField, BooleanField, ListField, DictField
+from datetime import datetime, timezone
+
+
+class PreferenceQuestion(Document):
+    content = ListField(DictField(), default=[])
+    is_active = BooleanField(default=True)
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+
+    
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now(timezone.utc)
+        return super(PreferenceQuestion, self).save(*args, **kwargs)
+
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "content": self.content,
+            "is_active": self.is_active,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
