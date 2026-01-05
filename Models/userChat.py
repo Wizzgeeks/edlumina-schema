0622137ff7b30ec46e7eafb64712137902dbb942
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-01-05 22:39:00 +0530

    user chat model created

--- Models/userChat.py
+++ Models/userChat.py
@@ -2,7 +2,7 @@ from Models.course_page_content import CoursePageContent
 from Models.subject_page_content import SubjectPageContent
 from Models.topic_page_content import TopicPageContent
 from Models.subtopic_page_content import SubtopicPageContent
-from mongoengine import Document, StringField, ReferenceField, ListField, DateTimeField, BooleanField, CASCADE
+from mongoengine import Document, StringField, ReferenceField, ListField, DateTimeField, DictField, CASCADE
 from datetime import datetime, timezone
 from Models.course import Course
 from Models.subject import Subject
@@ -20,7 +20,7 @@ class UserChat(Document):
     course_page_content = ReferenceField(CoursePageContent, reverse_delete_rule=CASCADE)
     topic_page_content = ReferenceField(TopicPageContent, reverse_delete_rule=CASCADE)
     subject_page_content = ReferenceField(SubjectPageContent, reverse_delete_rule=CASCADE)
-    messages = ListField(StringField())
+    messages = ListField(DictField())
     created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
     updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
 
