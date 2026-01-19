1c1467a83db29f1e964c8bf131ef0b5022ccbc9b
Rajasekar <rajasekar@wizzgeeks.com>
2026-01-19 10:45:58 +0530

    fixed the homework schema

--- Models/homework.py
+++ Models/homework.py
@@ -1,12 +1,21 @@
-from mongoengine import Document, ReferenceField, DateTimeField,ListField,DictField,CASCADE,NULLIFY
+from mongoengine import Document, ReferenceField, DateTimeField,ListField,DictField,CASCADE,NULLIFY,StringField
 from datetime import datetime, timezone
 from Models.user import Users
 from Models.institution_users import InstitutionUsers
 from Models.batches import Batches
+from Models.course import Course
+from Models.subject import Subject
+from Models.topic import Topic
+from Models.subtopic import Subtopic
 
 class Homework(Document):
     batch=ReferenceField(Batches,reverse_delete_rule=CASCADE)
     teacher=ReferenceField(InstitutionUsers,reverse_delete_rule=CASCADE)
+    name=StringField()
+    course=ReferenceField(Course,reverse_delete_rule=CASCADE)
+    subject=ReferenceField(Subject, reverse_delete_rule=CASCADE)
+    topic=ReferenceField(Topic, reverse_delete_rule=CASCADE)
+    subtopic=ReferenceField(Subtopic,reverse_delete_rule=CASCADE)
     content=ListField(DictField(),default=[])
     users = ListField(ReferenceField(Users, reverse_delete_rule=NULLIFY))
     deadline=DateTimeField()
