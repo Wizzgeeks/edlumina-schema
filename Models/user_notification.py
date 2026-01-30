e172b1f723a51e8399dd85fa424a4fa6f5b034ff
Abhay <abhaykumar@wizzgeeks.com>
2026-01-30 18:16:15 +0530

    added name in user notificaiton

--- Models/user_notification.py
+++ Models/user_notification.py
@@ -3,12 +3,12 @@ from Models.user import Users
 from datetime import datetime, timezone
 import uuid
 
-
 class Notification(EmbeddedDocument):
     notification_id = StringField(default=lambda: str(uuid.uuid4()))
     message = StringField(default="You have a new notification")
+    name=StringField(default="")
     notification_type = StringField(
-        choices=("homework", "reminder", "general"),
+        choices=("homework", "blog", "general","test"),
         default="general"
     )
     mark_as_read = BooleanField(default=False)
@@ -18,9 +18,10 @@ class Notification(EmbeddedDocument):
         return {
             "notification_id": self.notification_id,
             "message": self.message,
+            "name":self.name,
             "notification_type": self.notification_type,
             "mark_as_read": self.mark_as_read,
-            "created_at": self.created_at
+            "created_at": self.created_at.isoformat()
         }
 
 
