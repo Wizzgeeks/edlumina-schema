b863b087e6240b5e3c37bfc06a3e58943c37b1ba
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-02-02 10:50:59 +0530

    changes in the response to_json

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="edlumina-schema",
-    version="1.8.1",
+    version="1.8.2",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
