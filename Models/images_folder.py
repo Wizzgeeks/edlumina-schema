fca16acecf7934ecd36694024a5ad8876ff160ce
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-03-24 20:51:43 +0530

    changes in the  images folders  model

--- Models/images_folder.py
+++ Models/images_folder.py
@@ -6,3 +6,22 @@ class ImageFolder(Document):
     images_data=ListField(DictField())
     created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
     updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now(timezone.utc)
+        return super(ImageFolder, self).save(*args, **kwargs)
+
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "images_data": self.images_data,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+    def to_json_medium(self):
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
