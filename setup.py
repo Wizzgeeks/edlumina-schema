a83d6a37aff6907172253d2eccc28fb4bcfd856a
Selvaprasanth-16 <selvaprasanth@wizzgeeks.com>
2026-06-15 11:54:50 +0530

    added new test result for the question bank instution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="edlumina-schema",
-    version="2.0.7",
+    version="2.0.8",
     description="Schema for Flask Admin and User backends",
     author="wizzgeeks",
     author_email="",
