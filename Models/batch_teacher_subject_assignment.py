4e70fd97551796acab6a6e75aa51554dc41cd686
Abhay <abhaykumar@wizzgeeks.com>
2026-06-19 17:04:42 +0530

    added subject assignment or teacher colletion

---
+++ Models/batch_teacher_subject_assignment.py
@@ -0,0 +1,82 @@
+from mongoengine import (
+    Document,
+    ReferenceField,
+    DateTimeField,
+    CASCADE,
+    StringField
+)
+from datetime import datetime, timezone
+
+from Models.institution import Institution
+from Models.course import Course
+from Models.batches import Batches
+from Models.subject import Subject
+from Models.institution_users import InstitutionUsers
+
+
+class BatchTeacherSubjectAssignment(Document):
+    institution = ReferenceField(
+        Institution,
+        required=True,
+        reverse_delete_rule=CASCADE
+    )
+
+    course = ReferenceField(
+        Course,
+        required=True,
+        reverse_delete_rule=CASCADE
+    )
+
+    batch = ReferenceField(
+        Batches,
+        required=True,
+        reverse_delete_rule=CASCADE
+    )
+
+    subject = ReferenceField(
+        Subject,
+        required=True,
+        reverse_delete_rule=CASCADE
+    )
+
+    teacher = ReferenceField(
+        InstitutionUsers,
+        required=True,
+        reverse_delete_rule=CASCADE
+    )
+
+    created_at = DateTimeField(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    updated_at = DateTimeField(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    created_by = StringField()
+    updated_by = StringField()
+
+    meta = {
+        "indexes": [
+            ("batch", "subject"),
+            ("teacher",),
+            ("course",),
+            ("institution",)
+        ]
+    }
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now(timezone.utc)
+        return super().save(*args, **kwargs)
+
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "institution_id": str(self.institution.id),
+            "course_id": str(self.course.id),
+            "batch_id": str(self.batch.id),
+            "subject_id": str(self.subject.id),
+            "teacher_id": str(self.teacher.id),
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
